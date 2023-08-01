658ea0edf9c89aa11bfcfc2780ef8eb72f6c666e
Leidy Johanna Sánchez Zamora <124950194+L-J-Sanchez-Zamora@users.noreply.github.com>
2023-08-01 05:16:40 -0500

    Complete_Footer_and_NavBar

--- client/src/Views/Home/Home.jsx
+++ client/src/Views/Home/Home.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Home() {
 
     return (
-        <h1>It's Home 
+        <h1>It's Home ***
 
             NFT stands for Non-Fungible Token, a non-fungible token. Tokens are units of value that are assigned to a business model, such as cryptocurrencies. And it is that NFTs have a close relationship with cryptocurrencies, at least technologically, although they are opposites, since a Bitcoin is a fungible good, and an NFT is a non-fungible good, but in essence, they are like the two faces of a technological currency.
 
