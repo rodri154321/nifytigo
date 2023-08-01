35f76e19b8bd14111058a76ac85788d1b17ae49b
JoseManuelFrancoAlzate <josemanuelalzatefranco12@gmail.com>
2023-07-31 21:05:18 -0500

    flujo de collections

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,10 @@
 const { Router } = require("express");
 const router = Router();
 const usersRoutes = require('./usersRoutes')
-
-
+const collectionsRoutes = require('./collectionsRoutes')
 
 router.use('/users', usersRoutes)
+router.use('/collection', collectionsRoutes)
 
 
 module.exports = router;
