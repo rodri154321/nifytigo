982c2e88e4e2d709d1571abbe8dcc663222670bb
JoseManuelFrancoAlzate <josemanuelalzatefranco12@gmail.com>
2023-07-31 20:28:52 -0500

    server_user

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     sequelize.define('users', {
-        name: {
+        name: { 
             type: DataTypes.STRING,
             allowNull: false,
         },
