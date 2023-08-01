754a54ba7936066c7e3db18eac572f8e93fa8659
Brian Gelvez <briannn97@gmail.com>
2023-08-01 11:54:36 -0300

    models

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -2,9 +2,36 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     sequelize.define('users', {
+        id: {
+            type: DataTypes.UUID, // genera un id mas largo, ocupa mas espacio en la BDD pero es mas seguro, sin colisiones
+            defaultValue: DataTypes.UUIDV4, // Genera automáticamente un UUID único
+            primaryKey: true,
+            allowNull: false,
+        },
         name: {
             type: DataTypes.STRING,
             allwNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        cellPhone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        country: {
+            type: DataTypes.STRING,
+            allowNull: false,
         },
     });
 };
