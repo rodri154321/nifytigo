7b1fc3670b9a8a20e97afab0c6b5b47417e9691f
JoseManuelFrancoAlzate <josemanuelalzatefranco12@gmail.com>
2023-08-01 16:44:52 -0500

    route post

--- server/src/handlers/usersHandler.js
+++ server/src/handlers/usersHandler.js
@@ -1,19 +1,36 @@
-const {allUsers} = require('../controllers/userController')
+const {allUsers, getCreateUser} = require('../controllers/userController')
 
 const getUsersHandler = async(req,res)=>{
+
     try {
-        
-        const allUser = allUsers()
-
-    res.status(201).send(allUser)
-
-    } catch (error) {
-        res.status(500).json({error: error.message}) 
     
+    
+    
+     const results = await allUsers()  
+    
+    
+    res.status(200).json(results)
+    } catch (error) {
+        res.status(400).json({error:error.message})
     }}
     
+    const getCreateUsers = async(req,res)=>{
+
+        const {name,lastName,email,password,cellPhone,country} = req.body
+try {
+
+    const newUser = await getCreateUser(name,lastName,email,password,cellPhone,country)
+
+    res.status(200).json()
+    
+} catch (error) {
+res.status(400).json({error:error.message = 'No se creo el usuario'})
+}
+    }
+
 
     module.exports = {
         getUsersHandler,
+        getCreateUsers
      
     }
