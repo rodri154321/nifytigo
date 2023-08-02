d137a1778d55719178e780889702fcabbe20506f
Camilo <bcbjpro036crak@gmail.com>
2023-08-02 16:15:07 -0500

    error/arreglo

--- client/src/Views/Detail/Detail.jsx
+++ client/src/Views/Detail/Detail.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+/*falta que se haga el la subida del back
 
+import { useEffect, useState} from 'react'
+import {  useParams } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Detail() {
 
 
+    const { id } = useParams();
+    const [gameID, setGameID] = useState({})
+    useEffect(() => {
+        axios(`http://localhost:3001/videogames/${id}`).then(({ data }) => {
+           if (data.name) {
+            setGameID(data);
+           } else {
+              alert('No hay personajes con ese ID');
+           }
+        });
+        return setGameID({});
+     }, [0]);
+
 return (
 
-<div><h1>It's Detail</h1></div>
+<div>
+
+          <p>{gameID.id}</p>
+           <p>{gameID.name}</p> 
+           <img src={gameID.image} alt="" /> 
+           <p>description: {gameID.description}</p>
+</div>
 
     );
-}
+}*/
--- client/src/Views/Home/Home.jsx
+++ client/src/Views/Home/Home.jsx
@@ -1,12 +1,52 @@
+<<<<<<< Updated upstream
 import './home_styles.css';
 import React,{useEffect,useState} from 'react';
 import {useDispatch,useSelector} from 'react-redux';
+=======
+
+
+/* BRANDON
+import Cards from "../../Components/Cards/Cards";
+import  { useEffect,useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getEjemplo } from "../../Redux/getEjemplo"
+
+>>>>>>> Stashed changes
 
 // import NavBar from '../../Components/NavBar/NavBar';
 import Filters from '../../Components/Filters/Filters';
 import videoBackground from '../../assets/background_video/Waves.webm'
 import Pagination from '../../Components/Pagination/Pagination'
 
+<<<<<<< Updated upstream
+=======
+ const dispatch = useDispatch()
+ 
+
+const ejemplo = useSelector((state) => state.ejemplo)
+
+ const [currentPage] = useState(1);
+
+ const [videogamesPerPage] = useState(15);
+
+ const lastIndex = currentPage * videogamesPerPage; 
+ const firstIndex = lastIndex - videogamesPerPage;
+
+ const currentEjemplo= ejemplo.slice(firstIndex, lastIndex);
+ //EL SLICE 
+
+
+ useEffect(()=>{
+    dispatch(getEjemplo())
+   }, [dispatch])
+
+    return (
+        <div>
+        
+       
+        <h1>
+            It`s Home 
+>>>>>>> Stashed changes
 
 function Home(){
 
@@ -14,6 +54,7 @@ const [currentPage, setCurrentPage] = useState(1);
 const cardsPerPage=15;
 //const cardsFiltered = useSelector((state) => state.cardsFiltered);
 
+<<<<<<< Updated upstream
   //! Lógica para paginado
 //   const indexOfLastCard = currentPage * cardsPerPage;                                                             //Obtiene el index del ultimo juego p/pagina
 //   const indexOfFirstCard = indexOfLastCard - cardsPerPage;                                                        //Obtiene el index del primer juego p/pagina
@@ -37,3 +78,30 @@ const cardsPerPage=15;
 
 export default Home;
 
+=======
+        </h1>
+        <div >
+        
+        { currentEjemplo?.map((eje) =>{
+  
+      return(
+     
+            
+            <Cards
+            key={eje.id}
+            id={eje.id}
+            name={eje.name}
+            image={eje.image}
+            />
+        
+     
+      )
+       })}
+       
+        </div>
+        </div>
+        
+    );
+}
+*/
+>>>>>>> Stashed changes
