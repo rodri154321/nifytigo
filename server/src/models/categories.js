cefbbd933bfe0435c56e9ec7dd548889b36696e7
Brian Gelvez <briannn97@gmail.com>
2023-08-02 16:55:13 -0300

    models and relationships lists

--- server/src/models/categories.js
+++ server/src/models/categories.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-    sequelize.define('favorites', {
+    sequelize.define('categories', {
         id: {
             type: DataTypes.INTEGER, // genera un id mas largo, ocupa mas espacio en la BDD pero es mas seguro, sin colisiones
             defaultValue: DataTypes.UUIDV4, // Genera automáticamente un UUID único
