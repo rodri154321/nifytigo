56de5ec8b3ce915def6cb69821aea4a6563040c0
RodriAmaya <rodri154321mail.com>
2023-08-02 20:56:38 -0300

    rutas post n u

--- server/src/routes/nftRoutes.js
+++ server/src/routes/nftRoutes.js
@@ -6,13 +6,5 @@ const { getNftHandler, postNftHandler } = require('../handlers/nftHandler');
 router.get('/', getNftHandler);
 router.post('/create', postNftHandler);
 
-const { getNftHandler,postNftHandler }= require('../handlers/nftHandler')
-
-
-router
-.get('/', getNftHandler)
-router.post('/', postNftHandler)
-
-
 
 module.exports = router;
