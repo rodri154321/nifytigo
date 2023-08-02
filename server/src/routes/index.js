97bfcd030329c1a9eacda8586e4877cef89b0543
Camilo <bcbjpro036crak@gmail.com>
2023-08-02 15:28:10 -0500

    routes/api ejemplo

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const router = Router();
-const usersRoutes = require('./usersRoutes')
-const collectionsRoutes = require('./collectionsRoutes')
+const videoGamesRouter = require("./videoGamesRouter")
+//const usersRoutes = require('./usersRoutes')
+//const collectionsRoutes = require('./collectionsRoutes')
 
-router.use('/users', usersRoutes)
-router.use('/collection', collectionsRoutes)
+/*router.use('/users', usersRoutes)
+router.use('/collection', collectionsRoutes)*/
 
+router.use("/videogames", videoGamesRouter)
 
 module.exports = router;
