5d02df55e05f93a79fae5950c90a45e5d7a686d6
Camilo <bcbjpro036crak@gmail.com>
2023-08-04 13:10:39 -0500

    iniciarrutas

--- client/src/Views/About/About.jsx
+++ client/src/Views/About/About.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+
 //import style from "../../Views/About/About.module.css"
 
 export default function About() {
 
     return (
         <div>
-        <h1>It's About</h1>
+        <h1>Its About</h1>
         </div>
     );
 }
