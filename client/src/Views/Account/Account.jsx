4215f18d2b7dbd498db76e723e8a4a38b65a2fe8
DETNAW11 <alejito.prieto05@gmail.com>
2023-08-05 20:58:24 -0500

    AL

--- client/src/Views/Account/Account.jsx
+++ client/src/Views/Account/Account.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import './index.css'
+// Account.js
+import React, { useState } from 'react';
+import validation from './validation';
+import Login from '../Login/Login'
 
-const Account = () => {
+const Account = ({ onSubmit }) => {
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUserData((prevUserData) => ({
+      ...prevUserData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formErrors = validation(userData);
+    if (Object.keys(formErrors).length === 0) {
+      // Aquí puedes hacer la llamada al servidor o ejecutar cualquier acción con los datos
+      onSubmit(userData);
+    } else {
+      // Si hay errores, los establecemos en el estado para mostrarlos en el formulario
+      setErrors(formErrors);
+    }
+  };
+
+  const handleSignUp = () => {
+    window.location.href = Login;
+  };
+
   return (
-    <div className='AccountContainer'>
     <div className="main">
       <div className="container a-container" id="a-container">
-        <form className="form" id="a-form" method="" action="">
+        <form className="form" id="a-form" onSubmit={handleSubmit}>
           <h2 className="form_title title">Create Account</h2>
           <div className="form__icons">
             <img className="form__icon" src="" alt="" />
@@ -14,24 +46,90 @@ const Account = () => {
             <img className="form__icon" src="" />
           </div>
           <span className="form__span">or use email for registration</span>
-          <input className="form__input" type="text" placeholder="Name" />
-          <input className="form__input" type="text" placeholder="Email" />
-          <input className="form__input" type="password" placeholder="Password" />
-          <button className="form__button button submit">SIGN UP</button>
+          <input
+            className="form__input"
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={userData.name}
+            onChange={handleChange}
+          />
+          {errors.name && <div className="error">{errors.name}</div>}
+          <input
+            className="form__input"
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={userData.email}
+            onChange={handleChange}
+          />
+          {errors.email && <div className="error">{errors.email}</div>}
+          <input
+            className="form__input"
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={userData.password}
+            onChange={handleChange}
+          />
+          {errors.password && <div className="error">{errors.password}</div>}
+          <button className="form__button button submit" onClick={handleSubmit}>SIGN UP</button>
         </form>
       </div>
       <div className="switch" id="switch-cnt">
-        <div className="switch__circle"></div>
+      <div className="switch__circle"></div>
         <div className="switch__circle switch__circle--t"></div>
         <div className="switch__container" id="switch-c1">
           <h2 className="switch__title title">Welcome Back !</h2>
           <p className="switch__description description">To keep connected with us please login with your personal info</p>
-          <button className="switch__button button switch-btn">SIGN IN</button>
+          <button className="switch__button button switch-btn" onClick={handleSignUp}>SIGN IN</button>
         </div>
       </div>
-    </div>
     </div>
   );
 };
 
 export default Account;
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+
+// const Account = () => {
+//   return (
+//     <div className="main">
+//       <div className="container a-container" id="a-container">
+//         <form className="form" id="a-form" method="" action="">
+//           <h2 className="form_title title">Create Account</h2>
+//           <div className="form__icons">
+//             <img className="form__icon" src="" alt="" />
+//             <img className="form__icon" src="" />
+//             <img className="form__icon" src="" />
+//           </div>
+//           <span className="form__span">or use email for registration</span>
+//           <input className="form__input" type="text" placeholder="Name" />
+//           <input className="form__input" type="text" placeholder="Email" />
+//           <input className="form__input" type="password" placeholder="Password" />
+//           <button className="form__button button submit">SIGN UP</button>
+//         </form>
+//       </div>
+//       <div className="switch" id="switch-cnt">
+//         <div className="switch__circle"></div>
+//         <div className="switch__circle switch__circle--t"></div>
+//         <div className="switch__container" id="switch-c1">
+//           <h2 className="switch__title title">Welcome Back !</h2>
+//           <p className="switch__description description">To keep connected with us please login with your personal info</p>
+//           <button className="switch__button button switch-btn">SIGN IN</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Account;
--- client/src/Views/Login/Login.jsx
+++ client/src/Views/Login/Login.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
-import './index.css'
+import React, { useState } from 'react';
+import loginValidation from './loginValidation';
+import Account from '../Account/Account'
 
-const Login = () => {
+const Login = ({ onSubmit }) => {
+  const [userData, setUserData] = useState({
+    email: '',
+    password: '',
+  });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUserData((prevUserData) => ({
+      ...prevUserData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formErrors = loginValidation(userData);
+    if (Object.keys(formErrors).length === 0) {
+      // Aquí puedes hacer la llamada al servidor o ejecutar cualquier acción con los datos
+      onSubmit(userData);
+    } else {
+      // Si hay errores, los establecemos en el estado para mostrarlos en el formulario
+      setErrors(formErrors);
+    }
+  };
+  const handleSignUp = () => {
+    window.location.href = Account;
+  };
+
   return (
-    <div className='LoginContainer'>
     <div className="main">
       <div className="container b-container" id="b-container">
-        <form className="form" id="b-form" method="" action="">
+        <form className="form" id="b-form" onSubmit={handleSubmit}>
           <h2 className="form_title title">Sign in to Website</h2>
           <div className="form__icons">
             <img className="form__icon" src="" alt="" />
@@ -14,24 +43,80 @@ const Login = () => {
             <img className="form__icon" src="" />
           </div>
           <span className="form__span">or use your email account</span>
-          <input className="form__input" type="text" placeholder="Email" />
-          <input className="form__input" type="password" placeholder="Password" />
+          <input
+            className="form__input"
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={userData.email}
+            onChange={handleChange}
+          />
+          {errors.email && <div className="error">{errors.email}</div>}
+          <input
+            className="form__input"
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={userData.password}
+            onChange={handleChange}
+          />
+          {errors.password && <div className="error">{errors.password}</div>}
           <a className="form__link">Forgot your password?</a>
-          <button className="form__button button submit">SIGN IN</button>
+          <button className="form__button button submit" onClick={handleSubmit}>SIGN IN</button>
         </form>
       </div>
       <div className="switch" id="switch-cnt">
-        <div className="switch__circle"></div>
+      <div className="switch__circle"></div>
         <div className="switch__circle switch__circle--t"></div>
         <div className="switch__container" id="switch-c1">
           <h2 className="switch__title title">Hello Friend !</h2>
           <p className="switch__description description">Enter your personal details and start journey with us</p>
-          <button className="switch__button button switch-btn">SIGN UP</button>
+          <button className="switch__button button switch-btn" onClick={handleSignUp}>SIGN UP</button>
         </div>
       </div>
-    </div>
     </div>
   );
 };
 
 export default Login;
+
+
+
+
+
+
+
+
+
+// const Login = () => {
+//   return (
+//     <div className="main">
+//       <div className="container b-container" id="b-container">
+//         <form className="form" id="b-form" method="" action="">
+//           <h2 className="form_title title">Sign in to Website</h2>
+//           <div className="form__icons">
+//             <img className="form__icon" src="" alt="" />
+//             <img className="form__icon" src="" />
+//             <img className="form__icon" src="" />
+//           </div>
+//           <span className="form__span">or use your email account</span>
+//           <input className="form__input" type="text" placeholder="Email" />
+//           <input className="form__input" type="password" placeholder="Password" />
+//           <a className="form__link">Forgot your password?</a>
+//           <button className="form__button button submit">SIGN IN</button>
+//         </form>
+//       </div>
+//       <div className="switch" id="switch-cnt">
+//         <div className="switch__circle"></div>
+//         <div className="switch__circle switch__circle--t"></div>
+//         <div className="switch__container" id="switch-c1">
+//           <h2 className="switch__title title">Hello Friend !</h2>
+//           <p className="switch__description description">Enter your personal details and start journey with us</p>
+//           <button className="switch__button button switch-btn">SIGN UP</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Login;
