51541b3558d0c100cf163d7b10de55f586a51e7f
Leidy Johanna Sánchez Zamora <124950194+L-J-Sanchez-Zamora@users.noreply.github.com>
2023-08-07 07:51:15 -0500

    Mensaje del commit

--- client/src/Views/FormNft/FormNft.jsx
+++ client/src/Views/FormNft/FormNft.jsx
@@ -1,200 +1,160 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import style from "./FormNft.module.css";
-import validate from "./validate";
-import axios from "axios"
-import {getCategories} from '../../Redux/getCategories';
-//import { postNft } from "../../Redux/postNft";
+import { useDispatch, useSelector } from "react-redux";
+import { postNft } from "../../Redux/postNft"; // Asegúrate de importar las acciones correctas
+import { getCategories } from "../../Redux/getCategories";
+import style from "./FormNft.module.css"; // Importa los estilos CSS correspondientes
 
-export default function FormNft() {
-
-  const [form, setForm] = useState({
-    name: "",
-    categories: [],
-    description: "",
-    image: "",
-    price: "",
-    iduser: "b6274428-dbde-4bba-8019-ed45841a8a49",
-  });
-
-  const [errors, setErrors] = useState({
-    name: "",
-    categories: [],
-    description: "",
-    image: "",
-    price: "",
-  });
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    axios
-      .post(`http://localhost:3001/nft/create`, form)
-      .then((res) => alert('you have created your nft'))
-      .catch((err) => alert('you have not created your nft'))
-    setForm(
-      {
-        name: "",
-        categories: [],
-        description: "",
-        image: "",
-        price: "",
-      }
-    )
-  }
-
-  const handleChange = (event) => {
-    setForm({
-      ...form,
-      [event.target.name]: event.target.value,
-    });
-    setErrors(
-      validate({
-        ...form,
-        [event.target.name]: event.target.value,
-      })
-    );
-  };
-
-
+const FormNft = () => {
   const dispatch = useDispatch();
   const categories = useSelector((state) => state.categories);
 
+  const [formData, setFormData] = useState({
+    iduser: "72728d42-c637-4804-8aba-b710c11a22a5",
+    imageUrl: "",
+    name: "",
+    description: "",
+    price: "",
+    selectedCategories: [],
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    imageUrl: "",
+    name: "",
+    description: "",
+    price: "",
+  });
+
   useEffect(() => {
-    dispatch(getCategories())
+    dispatch(getCategories()); // Cargar las categorías al cargar el componente
   }, [dispatch]);
 
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
 
-  const handleCategories = (event) => {
-    if (!form.categories.includes(event.target.value)) {
-      setForm({
-        ...form,
-        categories: [...form.categories, event.target.value],
-      });
-      setErrors({
-        ...form,
-        categories: [...form.categories, event.target.value],
-      });
-    }
+    // Validaciones y actualización de errores
+    const errors = validateField(name, value);
+    setFormErrors({ ...formErrors, [name]: errors });
   };
 
+  const validateField = (fieldName, value) => {
+    let errors = "";
+    switch (fieldName) {
+      case "imageUrl":
+        // Validar la URL de la imagen con una expresión regular
+        if (!/^.{1,210}$/.test(value)) {
+          errors = "La URL debe tener hasta 210 caracteres y no debe contener espacios en blanco.";
+        }
+        break;
+      case "name":
+        // Validar el nombre con una expresión regular
+        if (!/^[A-Za-z]{3,25}$/.test(value)) {
+          errors = "El nombre debe tener entre 3 y 25 letras y no debe contener espacios en blanco.";
+        }
+        break;
+      case "description":
+        // Validar la descripción con la longitud máxima
+        if (value.length > 210) {
+          errors = "La descripción no debe exceder los 210 caracteres.";
+        }
+        break;
+      default:
+        break;
+    }
+    return errors;
+  };
 
+  const handleCategoryChange = (event) => {
+    const selectedOptions = Array.from(event.target.selectedOptions, (option) => option.value);
+    setFormData({ ...formData, selectedCategories: selectedOptions });
+  };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Llamar a la acción para enviar los datos al servidor
+    dispatch(postNft(formData));
+  };
 
   return (
-    <div className={style.containerPage}>
-      <form className={style.containerFormImg} onSubmit={handleSubmit}>
-        <h2>Create your NFT:</h2>
-        <div className={style.inputs}>
+    <div className={style.containerFormNft}>
+      <div className={style.containerMain}>
+        <h1>Crear NFT</h1>
+        <form onSubmit={handleSubmit}>
+          <h3 htmlFor="imageUrl">URL de la Imagen:</h3>
+          <input
+            type="text"
+            id="imageUrl"
+            name="imageUrl"
+            value={formData.imageUrl}
+            onChange={handleInputChange}
+            required
+            className={style.input}
+          />
+          {formErrors.imageUrl && <p className={style.error}>{formErrors.imageUrl}</p>}
 
-          <div className={style.image}>
-            {/************* Image *************/}
-            <input
-              type="url"
-              defaultvalue={form.image}
-              onChange={handleChange}
-              placeholder="Enter URL"
-              className={style.inputForm}
-            />
-            {errors.image && <p>{errors.image}</p>}
-          </div>
+          <h3 htmlFor="name">Nombre:</h3>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            required
+          />
+          {formErrors.name && <p className={style.error}>{formErrors.name}</p>}
+         
 
-          <div className={style.image}>
-            {/*************Name*************/}
-            {/* Cambiar `value` a `defaultValue` y agregar el controlador `onChange` */}
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}
-              placeholder="Name"
-              className={style.inputForm}
-            />
-            {errors.name && <p>{errors.name}</p>}
-          </div>
+          <h3 htmlFor="description">Descripción:</h3>
+          <textarea
+            id="description"
+            name="description"
+            value={formData.description}
+            onChange={handleInputChange}
+            rows="4"
+            required
+          ></textarea>
+           {formErrors.description && <p className={style.error}>{formErrors.description}</p>}
 
 
-          <div className={style.image}>
-            <select
-              name="categories"
-              value={categories}
-              onChange={handleCategories}
-              className={style.inputForm}
-            >
-              <option value="" disabled>
-                Categorías
+          <h3 htmlFor="price">Precio:</h3>
+          <input
+            type="number"
+            id="price"
+            name="price"
+            value={formData.price}
+            onChange={handleInputChange}
+            step="0.01"
+            required
+          />
+           {formErrors.price && <p className={style.error}>{formErrors.price}</p>}
+
+          <h3 htmlFor="categories">Categorías:</h3>
+          <select
+            multiple
+            id="categories"
+            name="selectedCategories"
+            value={formData.selectedCategories}
+            onChange={handleCategoryChange}
+            required
+          >
+            {categories.map((category) => (
+              <option key={category.id} value={category.name}>
+                {category.name}
               </option>
-              {categories.map((category) => (
-                <option key={category.id} value={category.name}>
-                  {category.name}
-                </option>
-              ))}
-            </select>
+            ))}
+          </select>
 
-            {/*   <option value="Art">Art</option>
-              <option value="Gaming">Gaming</option>
-              <option value="PFPs">PFPs</option>
-              <option value="Photography">Photography</option>
-  <option value="Music">Music</option> */}
-
-            {form.categories.length > 0 && (
-              <div className={style.selectedCategories}>
-                {form.categories.map((category, index) => (
-                  <div key={index} className={style.categoryTag}>
-                    {category}
-                    <button
-                      type="button"
-                      onClick={() => handleRemoveCategory(category)}
-                    >
-                      X
-                    </button>
-                  </div>
-                ))}
-              </div>
-            )}
-            {errors.categories && <p>{errors.categories}</p>}
-          </div>
-
-
-          <div className={style.image}>
-            {/************Price:*************/}
-            {/* Cambiar `value` a `defaultValue` y agregar el controlador `onChange` */}
-            <input
-              type="number"
-              min="0"
-              name="price"
-              value={form.price}
-              onChange={handleChange}
-              placeholder="Price"
-              className={style.inputForm}
-            />
-            {errors.price && <p>{errors.price}</p>}
-          </div>
-
-          <div className={style.image}>
-            {/************* Descripción:*************/}
-            {/* Cambiar `value` a `defaultValue` y agregar el controlador `onChange` */}
-            <textarea
-              //defaultValue={form.description}
-              name="description"
-              value={form.description}
-              onChange={handleChange}
-              placeholder="Description"
-              className={style.textAreaForm}
-            />
-            {errors.description && <p>{errors.description}</p>}
-          </div>
-
-          <div className={style.btnContainer}>
-
-            <button className={style.btn} type="submit">
-              Create
-            </button>
-          </div>
-        </div>
-      </form >
-    </div >
+          <button type="submit" className={style.button}>
+            Create NFT
+          </button>
+        </form>
+      </div>
+    </div>
   );
-}
+};
+
+export default FormNft;
 
 
 
