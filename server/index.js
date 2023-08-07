6fc48fb5d3c7b802318b5acb2d02d4ba0a0f1bb8
Brian Gelvez <briannn97@gmail.com>
2023-08-07 16:21:52 -0300

    cambios

--- server/index.js
+++ server/index.js
@@ -1,13 +1,8 @@
 const server = require("./src/server");
 const { conn } = require("./src/db");
 const PORT = 3001;
-<<<<<<< HEAD
-           //alter
-conn.sync({ force: true }).then(() => {
-=======
 
 conn.sync({ force : false }).then(() => {
->>>>>>> ddf6555a7edc71c98909e59ec800817363977b78
     server.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
     })
