823303b1f7fb6804cb49d054aa8effe945e7e5fc
Leidy Johanna Sánchez Zamora <124950194+L-J-Sanchez-Zamora@users.noreply.github.com>
2023-08-10 15:27:29 -0500

    correo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import { Route, Routes,/*useLocation */ } from 'react-router-dom'
 import './App.css'
 
-import videoBackground from './assets/Forms1080.webm'
+import videoBackground from './assets/Forms.webm'
+
 import Home from './Views/Home/Home'
 import Cards from './Components/Cards/Cards'
 import Detail from './Views/Detail/Detail'
@@ -9,11 +10,13 @@ import About from './Views/About/About'
 import NavBar from './Components/NavBar/NavBar'
 import Footer from './Components/Footer/Footer'
 import Contact from './Views/Contact/Contact'
-import Login from './Views/Login/Login'
+// import Login from './Views/Login/Login'
+import Login from './Views/LoginAuth0/LoginAuth0'
 import AboutProgrammers from './Views/AboutProgrammers/AboutPro'
 import FormNft from './Views/FormNft/FormNft'
 import Account from './Views/Account/Account'
 import PrivacyOfPolicy from './Views/PrivacyOfPolicy/PrivacyOfPolicy';
+import { Auth0Provider } from "@auth0/auth0-react";
 //import TermsOfService  from './Views/TermsOfService/TermsOfService'
 //import FrequentQuestions from './Views/FrequentQuestions/FrequentQuestions'
 
@@ -31,38 +34,35 @@ function App() {
           <Route path="/About" element={<About />} />
           <Route path="/Contact" element={<Contact />} />
           <Route path="/AboutProgrammers" element={<AboutProgrammers />} />
-          <Route path="/TermsOfService" element={<TermsOfService />} />
           <Route path="/detail/:id" element={<Detail />} />
-          
+          <Route path="/FormNft" element={<FormNft />} />
+          <Route path="/Account" element={<Account />} />
           <Route path="/PrivacyOfPolicy" element={<PrivacyOfPolicy />} />
-          <Route path='/Login' element={<Login />} />
+          {/* <Route path='/Login' element={<Login />} /> */}
+          <Route path='/Login' element={
+            <Auth0Provider
+              domain={domain}
+              clientId={clientId}
+              redirectUri={window.location.origin}
+            >
+              <Login />
+            </Auth0Provider>} />
 
-          
-        {/*
-          
-          <Route path="/PrivacyOfPolicy" element={<PrivacyOfPolicy />} />
-          
 
-
-
-           <Route path='/Login' element={<Login />} /> 
-          <Route path="/FormNft" element={<FormNft />} />      
-          <Route path="/FormCollection" element={<FormCollection />} />
-          <Route path="/Admin" element={<Admin />} />
-          <Route path="/Success" element={<Success />} />
-          <Route path="/Failure" element={<Failure />} />
-          
-          
-          <Route path="/FrequentQuestions" element={<FrequentQuestions />} />
-          <Route path="/Loader" element={<Loader />} />
-
-  */}
+          {/*
+        <Route path="/TermsOfService" element={<TermsOfService />}/>
+        <Route path="/FrequentQuestions" element={<FrequentQuestions />}/>
+        <Route path="/Admin" element={<Admin />}/>
+        <Route path="/Success" element={<Success />}/>
+        <Route path="/Failure" element={<Failure />}/>
+       */}
 
         </Routes>
-        
+
         <div id="containerFooter"><Footer /> </div>
+
       </div>
-      <video id='videoback' muted autoPlay loop> <source src={videoBackground} type="video/webm"/></video>
+      <video id='videoback' muted autoPlay loop> <source src={videoBackground} type="video/webm" /></video>
 
     </div>
   )
