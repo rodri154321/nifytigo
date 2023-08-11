383f58ee7e8b0dd928de3f37be6548a517695a09
JoseManuelFrancoAlzate <josemanuelalzatefranco12@gmail.com>
2023-08-11 16:35:37 -0500

    l

--- client/src/Views/Carrito/Carrito.jsx
+++ client/src/Views/Carrito/Carrito.jsx
@@ -1,7 +1,6 @@
 import  { useState, useEffect } from 'react';
 import { useSelector } from "react-redux"
 
-import axios from 'axios';
 
 const Carrito = () => {
   const {myFavorites} = useSelector(state=>state)
