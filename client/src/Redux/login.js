848fbe3bd5138f18362d9c014bae5771bee413d9
rodri154321 <rodri154321@gmail.com>
2023-08-14 17:15:04 -0300

    esto no se sube

--- client/src/Redux/login.js
+++ client/src/Redux/login.js
@@ -5,9 +5,9 @@ export const LOGIN = 'LOGIN';
 export const login = (user) => {
     return async function (dispatch) {
       try {
-         const response = await axios.post("http://localhost:3001/login", user);
-         console.log(response.data);
-      //const response = await axios.post("https://nifytigo.onrender.com/login", user);
+         //const response = await axios.post("http://localhost:3001/login", user);
+        
+      const response = await axios.post("https://nifytigoserver.onrender.com/login", user);
         
         return  dispatch({type: LOGIN, payload: response.data});
       } catch (error) {
