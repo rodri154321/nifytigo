b4ffbde1419811eacfe1c0873a732db9b6b4f8e1
RodriAmaya <rodri154321mail.com>
2023-08-15 18:23:34 -0300

    post nft user email

--- client/src/Redux/postNft.js
+++ client/src/Redux/postNft.js
@@ -6,7 +6,9 @@ export const postNft = (formData) => {
     try {
       //const response = await axios.post(`http://localhost:3001/nft/create`, formData);
       const idUs = localStorage.getItem('clientId')
+      console.log(idUs)
       const emailUser = (await axios.get(`https://nifytigoserver.onrender.com/users/${idUs}`)).data
+      console.log(emailUser);
       const {email} = emailUser; 
       console.log(email);
       const response = await axios.post(`https://nifytigoserver.onrender.com/nft/create/${email}`, formData);
