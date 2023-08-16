8ad18ac278c6d42ff61b9d060014cd2ba97b6970
rodri154321 <rodri154321@gmail.com>
2023-08-16 14:53:46 -0300

    cambio google controller

--- client/src/Redux/loginGoogle.js
+++ client/src/Redux/loginGoogle.js
@@ -7,6 +7,7 @@ export const loginGoogle = (user) => {
       // eslint-disable-next-line no-useless-catch
       try {
         //const response = await axios.post("http://localhost:3001/login/google", user);
+        console.log(user);
         const response = await axios.post("https://nifytigoserver.onrender.com/login/google", user);
         console.log(response.data.response[0]);
         return dispatch({type: LOGIN_GOOGLE, payload: response.data.response[0]});
--- server/src/controllers/loginGoogleController.js
+++ server/src/controllers/loginGoogleController.js
@@ -3,7 +3,7 @@ const { users } = require("../db.js");
 const postLoginGoogle = async(user) =>{
     console.log("EMAIL", user.email, "GOOGLEID", user.googleId, "NAME", user.name);
 
-    const user = await users.findOrCreate({ 
+    const newuser = await users.findOrCreate({ 
         where: { email : email },
         defaults: {
             name: user.name,
@@ -16,7 +16,7 @@ const postLoginGoogle = async(user) =>{
         } 
     });
 
-    return user;
+    return newuser;
     
 }
 
