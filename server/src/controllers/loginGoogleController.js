32eabfe8952afe54a10bcfc4aa3ad360c971cb63
rodri154321 <rodri154321@gmail.com>
2023-08-16 14:02:56 -0300

    consolelog logingoogle

--- server/src/controllers/loginGoogleController.js
+++ server/src/controllers/loginGoogleController.js
@@ -1,7 +1,7 @@
 const { User } = require("../db.js");
 
 const postLoginGoogle = async({email, googleId, name}) =>{
-
+    console.log("EMAIL", email, "GOOGLEID", googleId, "NAME", name);
 
     const user = await User.findOrCreate({ 
         where: { email : email },
