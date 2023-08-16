ab7e0276d2a984a19d12c1568d76b3f7eabdf01d
Brian Gelvez <briannn97@gmail.com>
2023-08-16 19:30:40 -0300

    yes

--- client/src/Redux/postNft.js
+++ client/src/Redux/postNft.js
@@ -1,83 +1,58 @@
-import axios from "axios";
-import { POST_NFT } from "./actionTypes"
-
-export const postNft = (formData) => {
-  return async function (dispatch) {
-    try {
-      const idUs = localStorage.getItem('clientId');
-
-      const userResponse = await axios.get(`https://nifytigoserver.onrender.com/users/${idUs}`);
-      const { email, name } = userResponse.data;
-
-      const nftResponse = await axios.post(`https://nifytigoserver.onrender.com/nft/create/${email}/${name}`, formData);
-
-      dispatch({ type: POST_NFT, payload: nftResponse.data });
-
-      return nftResponse.data; 
-    } catch (error) {
-      console.error("Error al enviar el formulario:", error);
-      throw error;
-    }
-  };
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // import axios from "axios";
-// import { POST_NFT } from "./actionTypes";
+// import { POST_NFT } from "./actionTypes"
 
 // export const postNft = (formData) => {
 //   return async function (dispatch) {
 //     try {
-//       //const response = await axios.post(`http://localhost:3001/nft/create`, formData);
-//       const idUs = localStorage.getItem('clientId')
-//       console.log(idUs)
+//       const idUs = localStorage.getItem('clientId');
 
+//       const userResponse = await axios.get(`https://nifytigoserver.onrender.com/users/${idUs}`);
+//       const { email, name } = userResponse.data;
 
-      
-//       const emailUser = (await axios.get(`https://nifytigoserver.onrender.com/users/${idUs}`)).data
-//       console.log(emailUser);
-//       const {email} = emailUser; 
-//       console.log(email);
-//       const response = await axios.post(`https://nifytigoserver.onrender.com/nft/create/${email}`, formData);
+//       const nftResponse = await axios.post(`https://nifytigoserver.onrender.com/nft/create/${email}/${name}`, formData);
 
-//       dispatch({ type: POST_NFT, payload: response.data });
+//       dispatch({ type: POST_NFT, payload: nftResponse.data });
 
-//       return response.data; 
+//       return nftResponse.data; 
 //     } catch (error) {
-
 //       console.error("Error al enviar el formulario:", error);
 //       throw error;
 //     }
 //   };
 // };
+
+
+
+
+
+import axios from "axios";
+import { POST_NFT } from "./actionTypes";
+
+export const postNft = (formData) => {
+  return async function (dispatch) {
+    try {
+      //const response = await axios.post(`http://localhost:3001/nft/create`, formData);
+      const idUs = localStorage.getItem('clientId')
+      console.log(idUs)
+
+
+      
+      const emailUser = (await axios.get(`https://nifytigoserver.onrender.com/users/${idUs}`)).data
+      console.log(emailUser);
+      const {email} = emailUser; 
+      console.log(email);
+      const response = await axios.post(`https://nifytigoserver.onrender.com/nft/create/${email}`, formData);
+
+      dispatch({ type: POST_NFT, payload: response.data });
+
+      return response.data; 
+    } catch (error) {
+
+      console.error("Error al enviar el formulario:", error);
+      throw error;
+    }
+  };
+};
 
 
 
