12722a1c99831eaee8ee94be6a7c417b822f8ed0
DETNAW11 <alejito.prieto05@gmail.com>
2023-08-18 15:31:54 -0500

    dash-prueba

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import {PayPalScriptProvider} from '@paypal/react-paypal-js'  //! After $ npm install @paypal/react-paypal-js
 import store  from './Redux/store.js';
+import DashboardView from './Views/Dashboard/DashboardView.jsx'; //prueva
 
 let clientID='Ac5Wfk9nx59rszQBbwzRFTkvK2jKMYdfY_Wf4tlMUWutbnLfMa5PH1ZCQpkxx8kZvSCSYgbfdXIqyTTI'   //! ID del usuario 
 
@@ -12,7 +13,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <BrowserRouter>
       <PayPalScriptProvider options={{"client-id":clientID}}>
-        <App />
+        {/* <App /> */}
+        <DashboardView/>
       </PayPalScriptProvider>
     </BrowserRouter>
   </Provider>,
