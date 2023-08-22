608162f63f7d071c9fb664783cac210106082c5c
Brian Gelvez <briannn97@gmail.com>
2023-08-22 16:30:26 -0300

    ruta baneo

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -46,10 +46,10 @@ module.exports = (sequelize) => {
             type: DataTypes.STRING(1000),
             allowNull: true,
         },
-        active: { // Nueva propiedad para controlar la activación/desactivación del usuario
-            type: DataTypes.BOOLEAN,
-            defaultValue: true, // Puedes configurarla en true por defecto para usuarios activos
-        },
+        // active: { // Nueva propiedad para controlar la activación/desactivación del usuario
+        //     type: DataTypes.BOOLEAN,
+        //     defaultValue: true, // Puedes configurarla en true por defecto para usuarios activos
+        // },
         customCreatedAt: {
             type: DataTypes.DATE,
             allowNull: false,
