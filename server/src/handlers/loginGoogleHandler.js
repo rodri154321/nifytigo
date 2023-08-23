5602b0568ff1c920562d8f626c7ee3261dc7c03f
Brian Gelvez <briannn97@gmail.com>
2023-08-23 00:26:02 -0300

    uf

--- server/src/handlers/loginGoogleHandler.js
+++ server/src/handlers/loginGoogleHandler.js
@@ -2,12 +2,11 @@ const { postLoginGoogle } = require("../controllers/loginGoogleController");
 
 
 const loginGoogle = async (req, res) => {
-  const {email, googleId,name,} = req.body;
+  const {email, googleId,name} = req.body;
   console.log("user", email,googleId,name);
   try {
 
     const response = await postLoginGoogle(email,googleId,name);
-
 
 
 
