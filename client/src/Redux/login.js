ceaa8935f415981c19f9d0cdd2d5da646ea6bf33
rodri154321 <rodri154321@gmail.com>
2023-08-23 13:04:11 -0300

    esto no se sube

--- client/src/Redux/login.js
+++ client/src/Redux/login.js
@@ -6,10 +6,10 @@ export const login = (iduser) => {
     return async function (dispatch) {
       // eslint-disable-next-line no-useless-catch
       try {
-         const response = (await axios.post("https://nifytigoserver.onrender.com/login/login", iduser).data);
+         const response = await axios.post("https://nifytigoserver.onrender.com/login/login", iduser);
      // const response = await axios.post("https://nifytigo.onrender.com/login", user);
         console.log(response);
-        return  dispatch({type: LOGIN, payload: response.user});
+        return  dispatch({type: LOGIN, payload: response.data.user});
       } catch (error) {
         throw error;
       }
