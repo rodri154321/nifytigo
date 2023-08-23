668d9eb1c12b79084a895301eb8d6129ab3783c2
Camilo <bcbjpro036crak@gmail.com>
2023-08-22 19:20:53 -0500

    b

--- server/src/routes/nftRoutes.js
+++ server/src/routes/nftRoutes.js
@@ -22,7 +22,10 @@ router
 .delete('/delete/:id', deleteNftHandler)
 .put('/:id', uptadeNftShop)
 .put('/update/:id', updateNftHandler)
+
 .get('/nfts/true',getNftTrueHandler)
+
 .get('/nfts/true/:userId',getNftTrueIdHandler)
+
 .get('/nfts/false',getNftFalseHandler)
 module.exports = router;
