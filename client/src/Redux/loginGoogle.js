040378fed21c07f6371b72f63d0956156f3a19de
rodri154321 <rodri154321@gmail.com>
2023-08-24 00:28:10 -0300

    google lo

--- client/src/Redux/loginGoogle.js
+++ client/src/Redux/loginGoogle.js
@@ -7,10 +7,10 @@ export const loginGoogle = (user) => {
       // eslint-disable-next-line no-useless-catch
       try {
         //const response = await axios.post("http://localhost:3001/login/google", user);
-        console.log(user);
-       const response = (await axios.post("https://nifytigoserver.onrender.com/login/google", user)).data
+        
+       const response = await axios.post("https://nifytigoserver.onrender.com/login/google", user)
         console.log(response);
-        return dispatch({type: LOGIN_GOOGLE, payload: response});
+        return dispatch({type: LOGIN_GOOGLE, payload: response.data});
       } catch (error) {
         throw error;
       }
