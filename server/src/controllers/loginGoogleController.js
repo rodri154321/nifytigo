1076d91a628652c0bb7ad310e3bbf4b8fc7ad1a9
rodri154321 <rodri154321@gmail.com>
2023-08-24 00:09:25 -0300

    google login

--- server/src/controllers/loginGoogleController.js
+++ server/src/controllers/loginGoogleController.js
@@ -3,26 +3,46 @@ const { users } = require("../db.js");
 const postLoginGoogle = async (email, googleId, name) => {
     console.log("EMAIL", email, "GOOGLEID", googleId, "NAME", name);
 
-    try {
-        const newuser = await users.findOrCreate({
-            where: { email: email },
-            defaults: {
-                name: name,
-                email: email,
-                password: googleId,
-                cellPhone: "",
-                country: "",
-                username: "",
-                lastName: "",
-            },
+    users.findOne({ where: { email: email } })
+  .then(existingUser => {
+    if (existingUser) {
+      return existingUser
+    } else {
+      // El usuario no existe, puedes crearlo
+      User.create({ email: email, name: name, password: googleId })
+        .then(newUser => {
+          return newUser
+        })
+        .catch(error => {
+            throw new Error(error , "no se pudo acceder")
         });
-
-        // Devuelve el nuevo usuario creado o el usuario existente
-        return newuser;
-    } catch (error) {
-        console.error("Error al crear/buscar usuario:", error);
-        throw error;
     }
+  })
+  .catch(error => {
+    throw new Error(error , "no se pudo acceder")
+  });
+
+
+    // try {
+    //     const newuser = await users.findOrCreate({
+    //         where: { email: email },
+    //         defaults: {
+    //             name: name,
+    //             email: email,
+    //             password: googleId,
+    //             cellPhone: "",
+    //             country: "",
+    //             username: "",
+    //             lastName: "",
+    //         },
+    //     });
+
+    //     // Devuelve el nuevo usuario creado o el usuario existente
+    //     return newuser[0];
+    // } catch (error) {
+    //     console.error("Error al crear/buscar usuario:", error);
+    //     throw error;
+    // }
 };
 
 module.exports = {
